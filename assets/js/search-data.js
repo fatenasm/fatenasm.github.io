a190d2447eced1cd39becf34f75e6744c4ac9d9e
fatenasm <103094490+fatenasm@users.noreply.github.com>
2026-02-03 23:23:50 +0000

    Deploying to gh-pages from @ fatenasm/fatenasm.github.io@f32a86feab05f9714bb895269d1abc45a9d581b8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -68,14 +68,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%66%32%36%32%39%30%30@%64%61%63.%75%6E%69%63%61%6D%70.%62%72", "_blank");
         },
       },{
         id: 'social-inspire',
         title: 'Inspire HEP',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://inspirehep.net/authors/", "_blank");
         },
       },{
         id: 'social-rss',
@@ -89,14 +89,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=PPBV5R8AAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
