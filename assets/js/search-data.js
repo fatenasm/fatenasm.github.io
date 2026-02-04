c37383a7516f56d7d01fd7c7e7a546f4f4f15465
fatenasm <103094490+fatenasm@users.noreply.github.com>
2026-02-04 00:44:42 +0000

    Deploying to gh-pages from @ fatenasm/fatenasm.github.io@bc250e192b30e04bdeaf46545a38c5a51e046be5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,11 +39,11 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "teachings-data-science-fundamentals",
-          title: 'Data Science Fundamentals',
-          description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
+          section: "News",},{id: "teachings-dm",
+          title: 'Dm',
+          description: "",
           section: "Teachings",handler: () => {
-              window.location.href = "/teachings/data-science-fundamentals.html";
+              window.location.href = "/teachings/DM.html";
             },},{id: "teachings-introduction-to-machine-learning",
           title: 'Introduction to Machine Learning',
           description: "This course provides an introduction to machine learning concepts, algorithms, and applications. Students will learn about supervised and unsupervised learning, model evaluation, and practical implementations.",
